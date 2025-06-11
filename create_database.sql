10baca9a44ca88b3ae3fb7d860d52b70b41e5e25
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2025-06-11 17:53:54 -0300

    Seguimos los criterios para nombrar las tablas y campos

--- create_database.sql
+++ create_database.sql
@@ -1,15 +1,15 @@
 use stockRepuestos;
 
-create table if not EXISTS  person (
+create table if not EXISTS person (
 	person_id int auto_increment not null unique, 
-	cuit varchar(11) not null, 
-	razon_social varchar(50) not null,
-	nombre varchar(20),
-	mail varchar(30) not null,
-	tel varchar(15) not null,
-	observaciones tinytext,
-	direccion varchar(20) not null,
-	impuestos enum("R.I",
+	tax_id varchar(11) not null, 
+	company_name varchar(50) not null,
+	name varchar(20),
+	email varchar(30) not null,
+	phone varchar(15) not null,
+	notes tinytext,
+	address varchar(20) not null,
+	tax_type enum("R.I",
 	"Exento",
 	"R.N.I",
 	"Monotributo",
@@ -23,68 +23,69 @@ create table if not EXISTS category(
 	description varchar(30)
 );
 
-create table products(
+create table product(
 	product_id int primary key auto_increment unique,
 	description varchar(50),
-	codigo varchar(20) not null,
+	code varchar(20) not null,
 	stock mediumint,
-	precio_compra float,
+	purchase_price float,
 	category_id int,
 	foreign key(category_id) references category(category_id)
 );
 
 
-create table empresas_transporte(
-	empresa_id int primary key auto_increment unique,
-	nombre varchar(30),
+create table transport_companies(
+	company_id int primary key auto_increment unique,
+	name varchar(30),
 	url varchar(40)
 );
 
-create table movimiento(
-	movimiento_id int primary key auto_increment unique,
-	fecha date,
-	compra_venta bool,
-	id_persona int,
-	transporte_id int, 
-	guia varchar(20),
-	impuesto enum("R.I",
-	"Excento",
+create table transaction(
+	transaction_id int primary key auto_increment unique,
+	date date,
+	is_sale bool,
+	person_id int,
+	transport_id int, 
+	tracking_number varchar(20),
+	tax_type enum("R.I",
+	"Exento",
 	"R.N.I",
 	"Monotributo",
 	"Consumidor Final"
 	),
-	foreign key(id_persona) references person(person_id),
-	foreign key(transporte_id) references empresas_transporte(empresa_id)	
+	foreign key(person_id) references person(person_id),
+	foreign key(transport_id) references transport_companies(company_id)	
 );
 
 create table items(
 	item_id int primary key auto_increment,
-	movimiento_id int,
+	transaction_id int,
 	product_id int,
-	cantidad mediumint,
-	precio float,
-	foreign key(movimiento_id) references movimiento(movimiento_id),
-	foreign key(product_id) references products(product_id)
-);
-create table extras(
-	extra_id int primary key auto_increment,
-	movimiento_id int not null,
-	precio float,
-	observacion varchar(50),
-	tipo enum("Mano de obra", "Envio", "Descuento"),
-	foreign key(movimiento_id) references movimiento(movimiento_id)
+	quantity mediumint,
+	price float,
+	foreign key(transaction_id) references transaction(transaction_id),
+	foreign key(product_id) references product(product_id)
 );
 
-create table pagos(
-	pago_id int primary key auto_increment,
-	movimiento_id int,
-	cantidad mediumint,
-	tipo enum(
-		"efectivo",
-		"transferencia",
-		"cheque",
-		"otro"
+create table extras(
+	extra_id int primary key auto_increment,
+	transaction_id int not null,
+	price float,
+	note varchar(50),
+	type enum("Mano de obra", "Envio", "Descuento"),
+	foreign key(transaction_id) references transaction(transaction_id)
+);
+
+create table payments(
+	payment_id int primary key auto_increment,
+	transaction_id int,
+	amount mediumint,
+	type enum(
+		"Efectivo",
+		"Transferencia",
+		"Cheque",
+		"Otro"
 	),
-	fecha date,
-	foreign key(movimiento_id) references movimiento(movimiento_id)
+	date date,
+	foreign key(transaction_id) references transaction(transaction_id)
 );
