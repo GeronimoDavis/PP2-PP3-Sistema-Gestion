1e0cc1662bea18c50b8c96d4fa2efb1c3ec161dd
WillianCorda <111395076+WillianCorda@users.noreply.github.com>
2025-07-29 22:58:27 -0300

    creado fontend de login
    no redirije a ninguna pagina solo muestra dato de usuario(id,token y nombre)

--- Servidor/public/index.php
+++ Servidor/public/index.php
@@ -2,11 +2,9 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-<<<<<<< Updated upstream
-=======
 use Dotenv\Dotenv;
 use Middlewares\CorsMiddleware;
->>>>>>> Stashed changes
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
